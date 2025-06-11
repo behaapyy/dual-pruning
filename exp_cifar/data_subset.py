2d2fc758170055cb4bcd30e8f0535a3ad35657bd
yeseul cho <91556597+behaapyy@users.noreply.github.com>
2025-06-11 16:34:53 +0900

    Update data_subset.py

--- exp_cifar/data_subset.py
+++ exp_cifar/data_subset.py
@@ -86,8 +86,6 @@ def load_cifar100_sub(args, target_probs=None, score=None, data_mask=None):
     ])
     
     train_data = dset.CIFAR100(args.data_path, train=True, transform=train_transform, download=True)
-    z = [[train_data.targets[i], score[np.where(data_mask == i)]] for i in range(len(train_data.targets))]
-    train_data.targets = z
     
     if args.sample == 'random':
         subset_mask = np.choice(50000, int(args.subset_rate* 50000), replace=False)
