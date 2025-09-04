326e7e3fb00b5e2a2d9b8cb18a2db2a33947899b
Yeseul <91556597+behaapyy@users.noreply.github.com>
2025-09-05 01:52:55 +0900

    Delete exp_cifar/models/commands.sh

--- exp_cifar/models/commands.sh
+++
@@ -1,5 +0,0 @@
-nohup python train.py --dataset cifar100 --arch resnet18 --batch-size 128 --dynamics --save_path ./save/resnet18-full/42 --manualSeed 42 --gpu 0 &
-nohup python train.py --dataset cifar100 --arch resnet18 --batch-size 128 --dynamics --save_path ./save/resnet18-full/43 --manualSeed 43 --gpu 1 &
-nohup python train.py --dataset cifar100 --arch resnet18 --batch-size 128 --dynamics --save_path ./save/resnet18-full/44 --manualSeed 44 --gpu 2 &
-nohup python train.py --dataset cifar100 --arch resnet18 --batch-size 128 --dynamics --save_path ./save/resnet18-full/45 --manualSeed 45 --gpu 3 &
-nohup python train.py --dataset cifar100 --arch resnet18 --batch-size 128 --dynamics --save_path ./save/resnet18-full/46 --manualSeed 46 --gpu 4 &
